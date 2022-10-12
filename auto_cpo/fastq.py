0173c26f1930d1647fc4b39e9db53cde58f614d4
Dan Fornika <dfornika@gmail.com>
2022-10-12 16:25:36 -0700

    Fix est read length (#8)
    * Adjust various report filenames, output dirs
    
    * Avoid crash when fewer than 100 reads available

--- auto_cpo/fastq.py
+++ auto_cpo/fastq.py
@@ -1,6 +1,7 @@
 import gzip
 import io
-
+import json
+import logging
 
 
 def get_first_n_reads(fastq_path, num_reads):
@@ -14,18 +15,23 @@ def get_first_n_reads(fastq_path, num_reads):
         f = open(fastq_path, 'r')
 
     while num_reads_read < num_reads:
-        header = next(f).strip()
-        seq = next(f).strip()
-        plus = next(f).strip()
-        quality = next(f).strip()
-        read = {
-            'header': header,
-            'seq': seq,
-            'quality': quality,
-        }
-        reads.append(read)
-        num_reads_read += 1 
+        try:
+            header = next(f).strip()
+            seq = next(f).strip()
+            plus = next(f).strip()
+            quality = next(f).strip()
+            read = {
+                'header': header,
+                'seq': seq,
+                'quality': quality,
+            }
+            reads.append(read)
+            num_reads_read += 1
+        except StopIteration as e:
+            break
+
     f.close()
+    logging.debug(json.dumps({"event_name": "sampled_reads_for_length_estimation", "fastq_path": fastq_path, "num_reads": len(reads)}))
 
     return reads
 
@@ -41,14 +47,16 @@ def estimate_read_length(reads):
     if num_reads > 0:
         avg_read_len = total_len_seqs / num_reads
 
-    if avg_read_len > 50 and avg_read_len < 100:
+    if avg_read_len > 52 and avg_read_len < 102:
         estimated_read_len = 100
-    elif avg_read_len > 100 and avg_read_len < 150:
+    elif avg_read_len > 102 and avg_read_len < 152:
         estimated_read_len = 150
-    elif avg_read_len > 150 and avg_read_len < 200:
+    elif avg_read_len > 152 and avg_read_len < 202:
         estimated_read_len = 200
-    elif avg_read_len > 150 and avg_read_len < 200:
+    elif avg_read_len > 152 and avg_read_len < 202:
         estimated_read_len = 250
+
+    logging.debug(json.dumps({"event_name": "estimated_read_length", "num_reads": num_reads, "mean_read_length": avg_read_len, "rounded_estimated_read_length": estimated_read_len}))
 
     return estimated_read_len
         
